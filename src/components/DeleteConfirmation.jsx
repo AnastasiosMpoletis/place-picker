5effd5fdf3a287840394aa67c0c37295aa591810
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-07 17:57:16 +0300

    L-187, 188,189. Close Modal after 3 seconds and delete Place automatically. Add a timer and useEffect

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    /**
+     * Cleanup function.
+     * It does not run right before the effect function is executed for the first time. 
+     * It is only executed right before subsequent executions of the effect function, or right before the component is removed from DOM.
+     */
+    return () => {
+      clearTimeout(timer);
+    };
+
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
