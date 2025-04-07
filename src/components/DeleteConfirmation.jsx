79a109548db69f2a75d11f3e9e4a0969fb280f75
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-07 20:07:40 +0300

    L-191. Implement Modal progress bar with useEffect to avoid infinite loop once again

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,10 +1,24 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime(previousTime => previousTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     /**
      * Cleanup function.
@@ -29,6 +43,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
